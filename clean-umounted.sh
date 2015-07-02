30d8448091c8027cbf4808b3f827b2a5c97d8782
Yang Ling <yangling1984@gmail.com>
2015-07-02 17:56:55 +0800

    Remove empty folder

--- clean-umounted.sh
+++ clean-umounted.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
+set -x
 
 /usr/bin/udevil clean
 
 MOUNT_ROOT=/media
+cd $MOUNT_ROOT
 
 for f in `ls $MOUNT_ROOT`; do
     ls $MOUNT_ROOT/$f
     isValidMount=$?
-    [[ $isValidMount -eq 0 ]] || { /usr/bin/udevil umount -l $MOUNT_ROOT/$f; }
+    if [[ $isValidMount -eq 0 ]]; then
+        # Delete this directory if it is empty
+        /usr/bin/rmdir $f
+    else
+        # Cannot access it. Try to umount it by lazy umount
+        /usr/bin/udevil umount -l $MOUNT_ROOT/$f
+    fi
 done
+
+set +x
