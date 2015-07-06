20700a50a487501e35dc5e3e1468f5459b458e13
Yang Ling <yangling1984@gmail.com>
2015-07-06 12:05:34 +0800

    Add dmenu search engine

---
+++ dmenu-search-engine.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+[[ -f $HOME/.dmenurc ]] && { . $HOME/.dmenurc;  }
+
+[[ -n "$DMENU" ]] || { DMENU='dmenu -nb #000000 -nf #ECDDA6 -sb #000000 -sf #b23308 -fn Ubuntu-16:bold -i '; }
+
+SEARCH_ENGINE="ciba
+google"
+
+result=$(echo -e "$SEARCH_ENGINE" | $DMENU -p "Choose search engine:")
+
+selected_engine="${result%% *}"
+
+search_content=$(echo "" | $DMENU -p "Input search content:" $*)
+
+case "$selected_engine" in
+    ciba)
+        xdg-open "http://www.iciba.com/$search_content/";;
+    google)
+        xdg-open "http://www.google.com/search?q=$search_content";;
+    *)
+        xdg-open "http://www.google.com/search?q=$search_content";;
+esac
