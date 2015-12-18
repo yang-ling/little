17f3fd64bd261b488f9e7096b7bce984c70eacaf
YANG Ling <yangling1984@gmail.com>
2015-12-18 19:39:21 +0800

    Add date for log

--- mail/sync-mails.sh
+++ mail/sync-mails.sh
@@ -6,24 +6,32 @@ sendWarning() {
     notify-send -t 300000 -u critical "$1" "$2"
 }
 
+adddate() {
+    while IFS= read -r line; do
+        echo "$(date +'%Z %F %T') $line"
+    done
+}
+
 LOCKFILE=/tmp/sync-mails-dotfile
 
+main() {
+    echo "Start Sync Mails!"
+    notify-send "Start Sync Mails!"
 
-echo "Start Sync Mails!"
-notify-send "Start Sync Mails!"
+    while true ; do
+        sleep 5m
+        dotlockfile -r 10 -l -p "$LOCKFILE"
 
-while true ; do
-    sleep 5m
-    dotlockfile -r 10 -l -p "$LOCKFILE"
+        [[ $? -ne 0 ]] && { sendWarning "Sync Mail" "Previous sync lasts too long!"; exit 1;}
 
-    [[ $? -ne 0 ]] && { sendWarning "Sync Mail" "Previous sync lasts too long!"; exit 1;}
+        mbsync -c ~/.config/isync/mbsyncrc -a
+        [[ $? -ne 0 ]] && { sendWarning "Sync Mail" "mbsync throws error!"; exit 1; }
 
-    mbsync -c ~/.config/isync/mbsyncrc -a
-    [[ $? -ne 0 ]] && { sendWarning "Sync Mail" "mbsync throws error!"; exit 1; }
+        offlineimap -c ~/.config/offlineimap/offlineimap.conf
+        [[ $? -ne 0 ]] && { sendWarning "Sync Mail" "offlineimap throws error!"; exit 1; }
 
-    offlineimap -c ~/.config/offlineimap/offlineimap.conf
-    [[ $? -ne 0 ]] && { sendWarning "Sync Mail" "offlineimap throws error!"; exit 1; }
+        dotlockfile -u "$LOCKFILE"
+    done
+}
 
-    dotlockfile -u "$LOCKFILE"
-done
-
+main | adddate
