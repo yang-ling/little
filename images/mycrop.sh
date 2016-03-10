d9fb8c6068159e279796ec6526dba56012ee7bb0
YANG Ling <yangling1984@gmail.com>
2016-03-10 18:42:47 +0800

    Improve image scripts

--- images/mycrop.sh
+++ images/mycrop.sh
@@ -2,4 +2,46 @@
 
 set -ex
 
-convert $1.jpg -crop 100%x$2% +repage $1%d.jpg
+getfilesize() {
+    filesize=$(stat -c%s "${1}")
+}
+
+recursive_crop() {
+    filename="${1%.*}"
+    convert $1 -crop 100%x50% +repage $filename%d.jpg
+    rm -f $1
+    for part in ${filename}0.jpg ${filename}1.jpg; do
+        filesize=0
+        getfilesize ${part}
+        if [ $filesize -ge 1000000 ]; then
+            recursive_crop ${part}
+        fi
+    done
+}
+
+rename_images() {
+    i=0
+    for file in $(ls *.jpg); do
+        newfilename="${1}_${i}.jpg"
+        if [ "$file" != "$newfilename" ]; then
+            mv $file $newfilename
+        fi
+        i=$((i+1))
+    done
+}
+
+mkdir temp
+
+convert $1.jpg -crop 100%x50% +repage temp/$1%d.jpg
+
+pushd temp
+
+for part in ${1}0.jpg ${1}1.jpg; do
+    filesize=0
+    getfilesize ${part}
+    if [ $filesize -ge 1000000 ]; then
+        recursive_crop ${part}
+    fi
+done
+rename_images $1
+popd
--- images/ymake_main.sh
+++ images/ymake_main.sh
@@ -13,37 +13,28 @@ i=0
 for file in $(ls *.jpg); do
     i=$((i+1))
     newfilename="${brand_name}_${product_name}_${suffix_main}_${i}${extension}"
-    mv $file $newfilename
+    if [ "$file" != "$newfilename" ]; then
+        mv $file $newfilename
+    fi
 done
 
 cd images
 i=0
-for file in $(ls .); do
+for file in $(ls *.jpg); do
     i=$((i+1))
     index="$i"
     if [ $i -lt 10 ]; then
         index="0${i}"
     fi
     newfilename="${brand_name}_${product_name}_${suffix_detail}_${index}${extension}"
-    mv $file $newfilename
+    if [ "$file" != "$newfilename" ]; then
+        mv $file $newfilename
+    fi
 done
 
 mergejpg.sh "${brand_name}_${product_name}_${suffix_detail}"
 
 filesize=$(stat -c%s "${brand_name}_${product_name}_${suffix_detail}.jpg")
-if [ $filesize -lt 1000000 ]; then
-    echo "Size is fine."
-elif [ $filesize -ge 1000000 ] && [ $filesize -lt 1400000 ]; then
-    mycrop.sh "${brand_name}_${product_name}_${suffix_detail}" 50
-elif [ $filesize -ge 1400000 ] && [ $filesize -lt 2400000 ]; then
-    mycrop.sh "${brand_name}_${product_name}_${suffix_detail}" 34
-elif [ $filesize -ge 2400000 ] && [ $filesize -lt 3400000 ]; then
-    mycrop.sh "${brand_name}_${product_name}_${suffix_detail}" 25
-elif [ $filesize -ge 3400000 ] && [ $filesize -lt 4400000 ]; then
-    mycrop.sh "${brand_name}_${product_name}_${suffix_detail}" 20
-elif [ $filesize -ge 4400000 ] && [ $filesize -lt 5400000 ]; then
-    mycrop.sh "${brand_name}_${product_name}_${suffix_detail}" 15
-else
-    echo "Too large!"
-    exit 1
+if [ $filesize -ge 1000000 ]; then
+    mycrop.sh "${brand_name}_${product_name}_${suffix_detail}"
 fi
