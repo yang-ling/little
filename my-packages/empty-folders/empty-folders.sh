cd8e42e9143ca50644ae6571f07a37a95fad2436
YANG Ling <yangling1984@gmail.com>
2016-05-20 09:22:12 +0800

    Add empty-folders package

--- my-packages/empty-folders/empty-folders.sh
+++ my-packages/empty-folders/empty-folders.sh
@@ -3,7 +3,13 @@
 set -ex
 
 emptyFolder() {
+    set +e
     find $1 -mindepth 1 -depth -mtime +7 -delete
+    result=$?
+    set -e
+    if [[ $result -ne 0 ]]; then
+        find $1 -mindepth 1 -depth -mtime +7 -exec rm -rf {} \;
+    fi
 }
 
 while IFS='' read -r line ; do
