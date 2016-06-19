a0d16bcade2a8cf4d0d250de7a4c6d902a47bcdf
YANG Ling <yangling1984@gmail.com>
2016-06-19 09:43:02 +0800

    Update clean-umount

--- my-packages/my-devmon/clean-umounted.sh
+++ my-packages/my-devmon/clean-umounted.sh
@@ -8,6 +8,10 @@ cd $MOUNT_ROOT
 
 for f in `ls $MOUNT_ROOT`; do
     /usr/bin/udevil umount -l $MOUNT_ROOT/$f
+    if [[ $? -ne 0 ]]; then
+        sudo /usr/bin/udevil umount -l $MOUNT_ROOT/$f
+    fi
+    [[ $? -ne 0 ]] && { echo "Error when unmounting $MOUNT_ROOT/$f"; exit 1; }
 done
 
 set +x
