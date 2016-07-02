b1df09b9f71b7ee61793c5e63a135eb250e64aef
YANG Ling <yangling1984@gmail.com>
2016-07-02 11:33:57 +0800

    Update installed-packages-list-timer

--- my-packages/installed-packages-list-timer/list-installed-packages.sh
+++ my-packages/installed-packages-list-timer/list-installed-packages.sh
@@ -10,7 +10,22 @@ whatihave-aur() {
     /usr/bin/pacman -Qemq
 }
 
-whatihave-official > $1-$(date +'%F').txt
-whatihave-aur > $2-$(date +'%F').txt
+main() {
+    line="${1}"
+    root_folder=$(echo "${line}" | cut -d ',' -f 1)
+    prefix=$(echo "${line}" | cut -d ',' -f 2)
+    [[ -d "${root_folder}" ]] || { echo "${root_folder} doesn't exist!"; exit 1; }
+    pushd "${root_folder}"
+    whatihave-official > ${prefix}-official-$(date +'%F').txt
+    whatihave-aur > ${prefix}-aur-$(date +'%F').txt
+    popd
+}
+
+
+while IFS='' read -r line ; do
+    [[ -z "${line}" ]] && continue
+    [[ "$line" =~ ^#.*$ ]] && continue
+    main "${line}"
+done < "$1"
 
 set +x
