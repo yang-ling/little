1b6df77421e0e29566d5ece1f4f9b1ca4432d725
YANG Ling <yangling1984@gmail.com>
2016-07-02 09:25:28 +0800

    Update video-util

--- my-packages/video-utils/myffconv.sh
+++ my-packages/video-utils/myffconv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 output_name="${1%.*}.mp4"
-ffmpeg -i $1 -vcodec libx264 -acodec libmp3lame -b:a 128K $output_name
+ffmpeg -i $1 -preset medium -c:v libx264 -profile:v main -crf 23 -qmin 18 -qmax 51 -maxrate 5000K -bufsize 7500K -c:a aac -strict experimental -b:a 128K -ar 44100 -ac 2 $output_name
