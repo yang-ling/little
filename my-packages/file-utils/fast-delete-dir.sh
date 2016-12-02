c0a5044d95077e1af3c44c0f09e5a152299c9fc2
YANG Ling <yangling1984@gmail.com>
2016-12-02 19:01:00 +0800

    Add fast-delete-dir tool

---
+++ my-packages/file-utils/fast-delete-dir.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+function echoHeader {
+    # Blue, underline
+    echo -e "\033[0;34;4m${1}\033[0m"
+}
+
+echoSection()
+{
+    echo -e "\033[47;30m${1}\033[0m"
+}
+echoInfo()
+{
+    # Green
+    echo -e "\033[0;32m${1}\033[0m"
+}
+echoError()
+{
+    # Red
+    echo -e "\033[0;31m${1}\033[0m"
+}
+
+usage="$(basename "$0") [options] directory_path
+
+options:
+    -h|--help                           show this help text"
+
+dirname=''
+
+options=$(getopt -o h --long "help" -- "$@")
+
+[ $? -eq 0 ] || {
+    echo "Incorrect options provided"
+    echo "$usage"
+    exit 1
+}
+
+eval set -- "$options"
+
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            echo "$usage"
+            exit 0
+            ;;
+        --)
+            dirname=$2
+            # In this case branch, the $2 may be empty and parameter could be only one, which means only `--'
+            # If so and if we do shift 2, the $# will always be 1, thus cause infinite loop.
+            # So we need check parameter number here and do proper shift.
+            # In other case branches, the $2 will never be empty because we use `getopt' and it can check it for us.
+            # So we needn't do such check in other case branches.
+            if [[ $# -eq 1 ]]; then
+                shift 1
+            else
+                shift 2;
+            fi
+            ;;
+        *)
+            # In most cases, `getopt' will check incorrect options for us, but if
+            # incorrect options appears after `--', `getopt' cannot detect that,
+            # so we need this case branch to catch such error.
+            echo "Incorrect options provided: $1"
+            echo "$usage"
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$dirname" ]; then
+    echo "No directory specified."
+    echo "$usage"
+    exit 1
+fi
+
+if [ ! -d "$dirname" ]; then
+    echo "$dirname is not a directory!"
+    echo "$usage"
+    exit 1
+fi
+
+echoHeader "Process Start!"
+
+# Remove trailing slash
+dirname=$(echo "$dirname" | sed 's@/$@@')
+
+echoInfo "Create temp directory."
+tempdir=$(mktemp -d)
+echoInfo "Temp directory is $tempdir"
+
+set -e
+
+rsync -a --delete --progress -h "$tempdir/" "$dirname/"
+rmdir "$dirname"
+
+set +e
+
+echoHeader "Process End!"
