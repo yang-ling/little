ea57ad0b74fefd1c6d03676120d1ac99d65f6955
YANG Ling <yangling1984@gmail.com>
2016-12-11 18:47:25 +0800

    Add my-notmuch-utils package.

---
+++ my-packages/my-notmuch-utils/delete-mails.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+DELETED_TAG_THRESHOLD=7
+SPAM_TAG_THRESHOLD=30
+function delete-mails-with-deleted-tag {
+    for oneMail in $(notmuch search --format=text --output=files tag:deleted); do
+        mail_age=$(perl -e 'print int -M $ARGV[0]' "$oneMail")
+        if [[ "$mail_age" -gt "$DELETED_TAG_THRESHOLD" ]]; then
+            echo "$oneMail has deleted tag and its age is $mail_age and threshold is $DELETED_TAG_THRESHOLD days. Delete it!"
+            rm -f "$oneMail"
+        else
+            echo "$oneMail has deleted tag and its age is $mail_age and threshold is $DELETED_TAG_THRESHOLD days. Do nothing"
+        fi
+    done
+}
+
+function delete-mails-with-spam-tag {
+    for oneMail in $(notmuch search --format=text --output=files tag:spam); do
+        mail_age=$(perl -e 'print int -M $ARGV[0]' "$oneMail")
+        if [[ "$mail_age" -gt "$SPAM_TAG_THRESHOLD" ]]; then
+            echo "$oneMail has spam tag and its age is $mail_age and threshold is $SPAM_TAG_THRESHOLD days. Delete it!"
+            rm -f "$oneMail"
+        else
+            echo "$oneMail has spam tag and its age is $mail_age and threshold is $SPAM_TAG_THRESHOLD days. Do nothing"
+        fi
+    done
+}
+
+delete-mails-with-deleted-tag
+delete-mails-with-spam-tag
---
+++ my-packages/my-notmuch-utils/my-send-mail.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/usr/bin/msmtp -C ~/.config/msmtp/msmtprc "$@"
