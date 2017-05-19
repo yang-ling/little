f52c8cfa8b252708b87e41cec36fa7e8d93a144c
YANG Ling <yangling1984@gmail.com>
2017-05-19 09:59:57 +0800

    Update qq-irc.

--- my-packages/my-irc/run-qq-local.sh
+++ my-packages/my-irc/run-qq-local.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/bin/vendor_perl/cpanm --local-lib=%h/perl5 local::lib && eval $(/usr/bin/perl -I %h/perl5/lib/perl5/ -Mlocal::lib)
+/usr/bin/vendor_perl/cpanm --local-lib=~/perl5 local::lib && eval $(/usr/bin/perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+/usr/bin/vendor_perl/cpanm Mojo::Webqq
+/usr/bin/vendor_perl/cpanm -v Mojo::IRC::Server::Chinese
 /usr/local/bin/qq.pl
