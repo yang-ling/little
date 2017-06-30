c08cb927b41d21c5df87e1c977c1cda00b7a848a
YANG Ling <yangling1984@gmail.com>
2017-06-30 17:24:49 +0800

    Use aac instead of libmp3lame

--- my-packages/video-utils/myff2pass.sh
+++ my-packages/video-utils/myff2pass.sh
@@ -7,4 +7,4 @@ extension="${1##*.}"
 
 basename="${1%.*}"
 
-ffmpeg -y -i $basename.mp4 -c:v libx264 -preset medium -b:v 555k -pass 1 -an -f mp4 /dev/null && ffmpeg -i $basename.mp4 -c:v libx264 -preset medium -b:v 555k -pass 2 -c:a libmp3lame -b:a 128k $basename-output.mp4
+ffmpeg -y -i $basename.mp4 -c:v libx264 -preset medium -b:v 555k -pass 1 -an -f mp4 /dev/null && ffmpeg -i $basename.mp4 -c:v libx264 -preset medium -b:v 555k -pass 2 -c:a aac -b:a 128k $basename-output.mp4
