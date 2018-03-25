4bb95f3982c9a6c40ec1b43e6cd52c16b53e8b1a
YANG Ling <yangling1984@gmail.com>
2018-03-25 12:39:53 +0800

    Update myffconv

--- my-packages/video-utils/myffconv.sh
+++ my-packages/video-utils/myffconv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-output_name="${1%.*}.mp4"
-ffmpeg -i $1 -preset medium -c:v libx264 -profile:v main -crf 23 -qmin 18 -qmax 51 -maxrate 5000K -bufsize 7500K -c:a aac -strict experimental -b:a 128K -ar 44100 -ac 2 $output_name
+orig_name="$@"
+output_name="${orig_name%.*}.mp4"
+ffmpeg -i "$orig_name" -preset medium -c:v libx264 -profile:v main -crf 23 -qmin 18 -qmax 51 -maxrate 5000K -bufsize 7500K -c:a aac -strict experimental -b:a 128K -ar 44100 -ac 2 "$output_name"
