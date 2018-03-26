79acf281583fe6b59df16a20b91f1cb1b6c17c74
YANG Ling <yangling1984@gmail.com>
2018-03-26 18:51:53 +0800

    Update myff2pass

--- my-packages/video-utils/myff2pass.sh
+++ my-packages/video-utils/myff2pass.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+orig_name="$@"
 
 # $1: mp4 file name
-extension="${1##*.}"
+extension="${orig_name##*.}"
 
 [[ "$extension" == "mp4" ]] || { echo "Only support mp4."; exit 1; }
 
-basename="${1%.*}"
+basename="${orig_name%.*}"
 
-ffmpeg -y -i $basename.mp4 -c:v libx264 -preset medium -b:v 555k -pass 1 -an -f mp4 /dev/null && ffmpeg -i $basename.mp4 -c:v libx264 -preset medium -b:v 555k -pass 2 -c:a aac -b:a 128k $basename-output.mp4
+ffmpeg -y -i "${basename}.mp4" -c:v libx264 -preset medium -b:v 555k -pass 1 -an -f mp4 /dev/null && ffmpeg -i "${basename}.mp4" -c:v libx264 -preset medium -b:v 555k -pass 2 -c:a aac -b:a 128k "${basename}-output.mp4"
